dd66aa9e5a35b7b1ce2123bcc88291b144781671
robscala <rob@scalasystems.com>
2020-11-13 18:00:14 -0500

    Complete the test class for DiscountApplier

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,45 @@
 package tudelft.discount;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class DiscountApplierTest {
 
+    @Test
+    public void basicTest()
+    {
+        MyProductDao myProductDao = new MyProductDao();
+        DiscountApplier discountApplier = new DiscountApplier(myProductDao);
+        discountApplier.setNewPrices();
+        for (Product product : myProductDao.all())
+        {
+            if (product.getCategory().equals("BUSINESS"))
+                Assertions.assertEquals(product.getPrice(), 110.0, 0.0001);
+            if (product.getCategory().equals("HOME"))
+                Assertions.assertEquals(product.getPrice(), 90.0, 0.0001);
+            if (product.getCategory().equals("OTHER"))
+                Assertions.assertEquals(product.getPrice(), 100.0, 0.0001);
+        }
+    }
+
+    private class MyProductDao implements ProductDao
+    {
+        private List<Product> products;
+
+        public List<Product> all()
+        {
+            if (products == null)
+            {
+                products = new ArrayList<>();
+                products.add(new Product("widget1", 100.0, "HOME"));
+                products.add(new Product("widget2", 100.0, "BUSINESS"));
+                products.add(new Product("widget3", 100.0, "OTHER"));
+            }
+            return products;
+        }
+    }
 }
